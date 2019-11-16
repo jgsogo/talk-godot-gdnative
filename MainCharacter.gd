ff13735761628cd1f6b7405ae2cc946a64b3ce31
Raul Huertas <raulh39@gmail.com>
2019-11-16 12:21:38 +0100

    Init

---
+++ MainCharacter.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+const MOTION_SPEED = 128 # Pixels/second
+
+
+func _physics_process(_delta):
+	var motion = Vector2()
+	
+	if Input.is_action_pressed("ui_up"):
+		motion += Vector2(0, -1)
+	if Input.is_action_pressed("ui_down"):
+		motion += Vector2(0, 1)
+	if Input.is_action_pressed("ui_left"):
+		motion += Vector2(-1, 0)
+	if Input.is_action_pressed("ui_right"):
+		motion += Vector2(1, 0)
+	
+	motion = motion.normalized() * MOTION_SPEED
+
+	move_and_slide(motion)
