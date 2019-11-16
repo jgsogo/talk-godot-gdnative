3643339b0f01d175b99244ca46ec8af0066c7ce3
Raul Huertas <raulh39@gmail.com>
2019-11-16 14:49:01 +0100

    Text sent is displayed

--- Dialog.gd
+++ Dialog.gd
@@ -1,14 +1,24 @@
 extends Area2D
 
+signal text_sent
 
 onready var msg_box := $PopupPanel/Contents/GhostMsg
+onready var input := $PopupPanel/Contents/Input
+
+func add_text(new_text:String) -> void:
+	msg_box.text += new_text + "\n"
 
 func _on_Dialog_body_entered(body):
 	$Ghost.visible = true
 	$PopupPanel.popup_centered()
-	msg_box.text = "epepepepepe"
 
 
 func _on_Dialog_body_exited(body):
 	$Ghost.visible = false
 	$PopupPanel.hide()
+
+
+func _on_SendButton_pressed():
+	msg_box.text += "$ " + input.text + "\n"
+	input.text = ""
+	emit_signal("text_sent")
